2db33cdebe2d6554cb6c835d7934caba969a190f
Ali Mirabzadeh <ali.mz758@gmail.com>
2019-02-19 11:03:17 -0800

    Update counter.v

--- counter.v
+++ counter.v
@@ -95,6 +95,7 @@ module counter(
             //increament sec0 if non of the above cases apply
 				secUnitDig_count <= secUnitDig_count + 4'b0001;
 			end
+			//if counter is at 59:59, make all the digits zero
 			if( minTensDig_count==4'b0101 && minUnitDig_count==4'b1001 && secTensDig_count==4'b0101 && secUnitDig_count==4'b1001)begin
             			//make all the digit counters to be zero
            			 minTensDig_count <= 4'b0000;
